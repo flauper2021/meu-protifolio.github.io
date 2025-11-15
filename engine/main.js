db5fecc0a4c02082d7157cbe913d747a5f925ef2
mauricioarrigada <mauricio.work@yahoo.com>
2025-11-14 21:02:09 -0300

    seção do sobre mim finalizada

--- engine/main.js
+++ engine/main.js
@@ -1,6 +1,7 @@
 const toggleTheme = document.getElementById('toggleTheme');
 const rootHtml = document.documentElement;
 
+
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
@@ -10,5 +11,26 @@ function changeTheme() {
 }
 
 toggleTheme.addEventListener("click", changeTheme);
-    
 
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item => {
+    item.addEventListener('click', () => {
+        menuLinks.forEach(item => item.classList.remove('active'));
+        item.classList.add('active');
+    })
+} 
+);
+
+
+/*-- Accordions --*/
+
+const accordionHeaders = document.querySelectorAll('.accordion__header');
+
+accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const accordionItem = header.parentElement;
+        accordionItem.classList.toggle('active');
+    })
+})
